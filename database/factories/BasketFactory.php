aa31fbe7b39fd05640871e96351cf649d3904871
TheNemoKing <115928048+TheNemoKing@users.noreply.github.com>
2023-05-03 14:23:38 +0200

    Empty Factories

--- database/factories/BasketFactory.php
+++ database/factories/BasketFactory.php
@@ -5,9 +5,9 @@ namespace Database\Factories;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
 /**
- * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\User>
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Basket>
  */
-class UserFactory extends Factory
+class BasketFactory extends Factory
 {
     /**
      * Define the model's default state.
--- database/factories/FoodFactory.php
+++ database/factories/FoodFactory.php
@@ -5,9 +5,9 @@ namespace Database\Factories;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
 /**
- * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\User>
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Food>
  */
-class UserFactory extends Factory
+class FoodFactory extends Factory
 {
     /**
      * Define the model's default state.
