c9b3cdf26b1e7421b4579d6c73accf7432f0e771
Dávid Kunovics <kunovicsdavid@gmail.com>
2023-05-12 16:41:42 +0200

    User available for all views

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\Auth;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -19,6 +20,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        view()->composer('*', function($view)
+        {
+            $view->with(['user' => Auth::user()]);
+        });
     }
 }
