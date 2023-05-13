4da45cc1e79982814e78ed161a644549e9e3e589
TheNemoKing <115928048+TheNemoKing@users.noreply.github.com>
2023-05-13 13:35:39 +0200

    New navbar

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -12,8 +12,6 @@
     <link href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@48,400,0,0"
         rel="stylesheet">
     <link rel="stylesheet" href="{{ Vite::asset('resources/css/style.css') }}">
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.4.1/jquery.min.js" charset="utf-8"></script>
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.12.1/css/all.min.css">
 </head>
 
 <style>
@@ -27,24 +25,43 @@
 </style>
 
 <body>
-    <header>
-        <div class="inner-width">
-          <h1 class="logo">Food<span style="color:#273b91;">panda</span>Kopia</h1>
-          <i class="menu-toggle-btn fas fa-bars"></i>
-          <nav class="navigation-menu">
-            <a href="{{ url('/') }}"><i class="fas fa-home home"></i> Kezdőlap</a>
-            <a href="{{ url('/restaurants') }}"></i> Éttermek</a>
-            <a href="{{ url('/restaurantmanager') }}"></i> Étterem manager</a>
-            <a href="#"></i>Kosár</a>
-            <a href="#"></i> Rólunk</a>
-            @if ($user != null)
-            <a href="#" class="aj_btn"> <i class="fas fa-lock" aria-hidden="true"></i>{{ $user->name }}</a>
-             @else
-             <a href="{{ url('/login') }}" class="aj_btn"> <i class="fas fa-lock" aria-hidden="true"></i>Bejelentkezés</a>
-            @endif
-          </nav>
-        </div>
-    </header>
+    <nav class="navbar">
+        <!-- LOGO -->
+        <div class="compname"><h1 class="logo">Food<span style="color:#273b91;">panda</span>Kópia</h1></div>
+        <!-- NAVIGATION MENU -->
+        <ul class="nav-links">
+            <!-- USING CHECKBOX HACK -->
+            <input type="checkbox" id="checkbox_toggle" />
+            <label for="checkbox_toggle" class="hamburger">&#9776;</label>
+            <!-- NAVIGATION MENUS -->
+            <div class="menu">
+                <li><a href="{{ url('/') }}">Kezdőoldal</a></li>
+                <li><a href="{{ url('/restaurants') }}">Éttermek</a></li>
+                <li><a href="{{ url('/restaurantmanager') }}">Étteremkezelő</a></li>
+                <li><a href="#">Rólunk</a></li>
+                <li><a href="#">Kosár</a></li>
+                @if ($user != null)
+                <li class="services">
+                    <a href="#">{{ $user->name }}</a>
+                    <!-- DROPDOWN MENU -->
+                    <ul class="dropdown">
+                        <li>
+                            <form method="POST" action="{{ route('logout') }}">
+                            @csrf
+                            <div>
+                                <input type="submit" value="Kijelentkezés"><br>
+                            </div>
+                        </form></li>
+                    </ul>
+                </li>
+                @else
+                <li><a href="{{ url('/login') }}">Bejelentkezés</a></li>
+                @endif
+                </div>
+
+        </ul>
+
+    </nav>
     <div>
         @yield('content')
     </div>
