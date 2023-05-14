54627ece321f880fcc1bc1df0dffb0cc57b289ed
TheNemoKing <115928048+TheNemoKing@users.noreply.github.com>
2023-05-14 17:45:37 +0200

    Restaurant manager login and register styling

--- resources/views/restaurantmanager.blade.php
+++ resources/views/restaurantmanager.blade.php
@@ -81,39 +81,54 @@
             <input type="submit" value="Lead">
         </form>
     @else
+        <h1 class="text-center text-4xl mt-6">Bejelentkezés</h1>
         @if (isset($restaurants))
-            @foreach ($restaurants as $restaurant)
-                <p>
-                    {{ $restaurant->name }}
-                </p>
-                <form method="POST" action="{{ route('restaurantmanager.login') }}">
-                    @csrf
-                    <div>
-                        <label for="password">
-                            Jelszó:
-                        </label>
-                        <input type="text" name="password">
+            <div class="flex items-center flex-wrap justify-around">
+                @foreach ($restaurants as $restaurant)
+                    <div class="w-1/2">
+                        <div
+                            class="font-regular relative rounded-lg m-8 hover:scale-110 bg-gradient-to-tr from-[#9128ed] to-[#ff83e2] p-4 text-base leading-5 text-white opacity-100">
+                            <h2 class="align-middle text-center mb-4 font-bold text-4xl">{{ $restaurant->name }}</h2>
+                            <form method="POST" action="{{ route('restaurantmanager.login') }}">
+                                @csrf
+                                <div class="hover:scale-105">
+                                    <label class="mr-2" for="password">
+                                        Jelszó:
+                                    </label>
+                                    <input class="text-black" type="text" name="password">
+                                </div>
+                                <div class="mt-4 text-center hover:scale-105 hover:text-pink-200">
+                                    <input class="outline-white rounded-lg outline outline-2 outline-offset-4"
+                                        type="submit" value="BEJELENTKEZÉS">
+                                </div>
+                                <input type="hidden" name="restaurant_id" value="{{ $restaurant->id }}">
+                            </form>
+                        </div>
                     </div>
-                    <div>
-                        <input type="submit" value="BEJELENTKEZÉS">
-                    </div>
-                    <input type="hidden" name="restaurant_id" value="{{ $restaurant->id }}">
-                </form>
-            @endforeach
+                @endforeach
+            </div>
         @endif
-        <form method="POST" action="{{ route('restaurantmanager.register') }}">
-            @csrf
-            <div>
-                <label for="name">Name</label>
-                <input type="text" name="name"><br>
+        <hr class="w-1/2 h-1 mx-auto my-4 bg-gray-100 border-0 rounded md:my-10 dark:bg-gray-700">
+        <h1 class="text-center text-4xl">Étterem regisztráció</h1>
+        <div class="flex items-center flex-wrap justify-around">
+            <div
+                class="flex items-center flex-wrap justify-around w-1/2 rounded-lg m-8 hover:scale-105 bg-gradient-to-tr from-[#9128ed] to-[#ff83e2] p-4 text-base leading-5 text-white opacity-100">
+                <form method="POST" action="{{ route('restaurantmanager.register') }}">
+                    @csrf
+                    <div class="hover:scale-105">
+                        <label class="mr-7" for="name">Étterem neve:</label>
+                        <input class="text-black" type="text" name="name"><br>
+                    </div>
+                    <div class="mt-4 hover:scale-105">
+                        <label class="mr-1" for="password">Étterem jelszava:</label>
+                        <input class="text-black" type="text" name="password"><br>
+                    </div>
+                    <div class="mt-8 text-center hover:scale-105 hover:text-pink-200">
+                        <input class="outline-white rounded-lg outline outline-2 outline-offset-4" type="submit"
+                            value="REGISZTRÁCIÓ"><br>
+                    </div>
+                </form>
             </div>
-            <div>
-                <label for="password">Password</label>
-                <input type="text" name="password"><br>
-            </div>
-            <div>
-                <input type="submit" value="REGISZTRÁCIÓ"><br>
-            </div>
-        </form>
+        </div>
     @endif
 @endsection
