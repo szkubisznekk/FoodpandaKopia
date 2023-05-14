fd5acfd167288825a7d20ff3e02525d9f80e04b2
TheNemoKing <115928048+TheNemoKing@users.noreply.github.com>
2023-05-14 22:00:51 +0200

    Cart style just started

--- resources/views/cart.blade.php
+++ resources/views/cart.blade.php
@@ -2,7 +2,9 @@
 
 @section('content')
     @if (Session::has('cart'))
+    <div class="flex items-center flex-wrap justify-around">
         @foreach (Session::get('cart') as $cartItem)
+        <div class="w-1/4 h-56">
             <p> Name: {{ strval($cartItem['food_name']) }} </p>
             <p> Amount: {{ strval($cartItem['amount']) }} db </p>
             <p> Price: {{ strval($cartItem['price']) }} Ft </p>
@@ -13,7 +15,9 @@
                 <input type="submit" value="Kosárból kivenni">
             </form>
             <br>
+        </div>
         @endforeach
+    </div>
         <form method="POST" action="{{ route('cart.emptyCart') }}">
             @csrf
             <input type="submit" value="Kosár ürítés">
@@ -24,5 +28,7 @@
                 <input type="submit" value="Tovább">
             </form>
         @endif
+    @else
+        <h2>Nincs semmi a kosaradban!</h2>
     @endif
 @endsection
