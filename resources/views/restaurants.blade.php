9b4d434173610d95cfc6d2bbc41cecac3630faf8
TheNemoKing <115928048+TheNemoKing@users.noreply.github.com>
2023-05-14 17:03:15 +0200

    Further formatting on food listing

--- resources/views/restaurants.blade.php
+++ resources/views/restaurants.blade.php
@@ -15,16 +15,16 @@
     @else
         <div>
             <form action="{{ url('restaurants/' . strval($pickedRestaurant)) }}">
-                <div class="relative border-2 border-gray-100 m-4 rounded-lg">
+                <div class="pl-4 align-middle text-center content-center relative border-2 border-gray-100 m-4 rounded-lg">
                     <div class="absolute top-4 left-3">
                         <i class="fa fa-search text-gray-400 z-20 hover:text-gray-500"></i>
                     </div>
                     <input type="text" name="search"
                         class="h-14 w-full p1-10 pr-20 rounded-lg z-0 focus:shadow focus:outline-none"
-                        placeholder="Search" />
+                        placeholder="Keresés" />
                     <div class="absolute top-2 right-2">
                         <button type="submit"
-                            class="h-10 w-20 text-white rounded-lg bg-gradient-to-tr from-[#9128ed] to-[#ff83e2]  hover:bg-red-600">Search</button>
+                            class="h-10 w-20 text-white rounded-lg bg-gradient-to-tr from-[#9128ed] to-[#ff83e2]  hover:bg-red-600">Keresés</button>
                     </div>
                 </div>
             </form>
@@ -32,7 +32,7 @@
         <div class="flex items-center flex-wrap justify-around">
             @foreach ($categories as $category)
                 <p
-                    class="font-regular align-middle text-center relative block w-full rounded-lg bg-pink-500 p-4 text-base leading-5 text-black opacity-100">
+                    class="font-regular hover:text-white tracking-widest hover:underline underline-offset-8 align-middle text-center font-bold relative block w-full rounded-lg bg-pink-500 p-4 text-2xl leading-5 text-black opacity-100">
                     {{ $category->name }} </p>
                 @foreach ($foods[$category->id] as $food)
                     <div class="m-auto w-1/2 h-auto">
@@ -50,11 +50,11 @@
                                     <div class="mt-4">
                                         <label class="font-bold">Darab: </label>
                                         <input
-                                            class="inline-block text-bs font-bold bg-pink-300 border border-gray-300 text-gray-900 rounded-lg focus:ring-blue-500 focus:border-blue-500 w-[80] p-2.5 dark:bg-pink-600 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                                            class="inline-block text-bs font-bold bg-pink-300 border border-gray-300 text-gray-900 rounded-lg focus:ring-pink-500 focus:border-pink-500 w-[80] p-2.5 dark:bg-pink-600 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-pink-500 dark:focus:border-pink-500"
                                             type="number" min="1" name="amount">
 
                                         <input
-                                            class=" ml-10 text-bs middle none center rounded-lg bg-pink-500 py-4 px-6 font-sans font-bold uppercase text-white shadow-md shadow-pink-500/20 transition-all hover:shadow-lg hover:shadow-pink-500/40 focus:opacity-[0.85] focus:shadow-none active:opacity-[0.85] active:shadow-none disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none"
+                                            class=" hover:text-pink-500 hover:bg-gray-200 ml-10 text-bs middle none center rounded-lg bg-pink-500 py-4 px-6 font-sans font-bold uppercase text-white shadow-md shadow-pink-500/20 transition-all hover:shadow-lg focus:opacity-[0.85] focus:shadow-none active:opacity-[0.85] active:shadow-none disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none"
                                             data-ripple-light="true" type="submit" value="Kosárba">
                                     </div>
                                 </div>
