9e8ad9e2f2467a57e021773424356dba5a28be96
TheNemoKing <115928048+TheNemoKing@users.noreply.github.com>
2023-05-15 14:00:58 +0200

    Cart page fix

--- resources/views/cart.blade.php
+++ resources/views/cart.blade.php
@@ -42,10 +42,29 @@
                             class="w-64 text-center hover:border-2 hover:border-red-800 hover:text-red-800 hover:bg-gray-200 text-sm rounded-lg bg-red-500 py-4 px-6 font-sans font-bold uppercase text-white transition-all hover:shadow-lg focus:opacity-[0.85] focus:shadow-none active:opacity-[0.85] active:shadow-none disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none"
                             type="submit" value="Tovább">
                     </form>
+                @else
+                    <a href="{{ url('auth') }}">
+                        <div
+                            class="w-64 text-center hover:border-2 hover:border-red-800 hover:text-red-800 hover:bg-gray-200 text-sm rounded-lg bg-red-500 py-4 px-6 font-sans font-bold uppercase text-white transition-all hover:shadow-lg focus:opacity-[0.85] focus:shadow-none active:opacity-[0.85] active:shadow-none disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none">
+
+                            <p>Bejelentkezés</p>
+
+                        </div>
+                    </a>
                 @endif
+
             </div>
         </div>
     @else
-        <h2>Nincs semmi a kosaradban!</h2>
+        <div class="flex items-center flex-wrap justify-around">
+            <a href="{{ url('/restaurants') }}">
+                <div
+                    class="font-bold hover:bg-gradient-to-r hover:from-purple-400 hover:to-pink-600 text-white hover:text-white bg-purple-600 bg-of outline outline-pink-400 my-8 text-2xl text-center hover:scale-105 font-regular relative m-1 p-4 block rounded-lg bg-gradient-to-tr from-[#9128ed] to-[#ff83e2] opacity-100">
+
+                    <h1 class="m-4 text-white">Nincs semmi a kosaradban</h1>
+
+                </div>
+            </a>
+        </div>
     @endif
 @endsection
