c0fa18f9d5db385a1f4feb5363bed3264bc45753
Dávid Kunovics <kunovicsdavid@gmail.com>
2023-06-02 10:32:13 +0200

    Added password to yourier

---
+++ database/migrations/2023_06_02_102653_add_password_to_couriers_table.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('couriers', function (Blueprint $table) {
+            $table->string('password');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('couriers', function (Blueprint $table) {
+            $table->dropColumn('password');
+        });
+    }
+};
